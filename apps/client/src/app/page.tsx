0e38921b7be0a27a08faf757325b891a5b978314
Jugwang <hjjk0721@gmail.com>
2025-05-13 02:11:00 +0900

    fix(tailwind): updating tailwind config for next V15 and tailwind v4

--- apps/client/src/app/page.tsx
+++ apps/client/src/app/page.tsx
@@ -1,3 +1,3 @@
 export default function Page() {
-  return <div className="text-3xl text-frippy-blue">Tailwind 작동 확인</div>;
+  return <div className="text-3xl text-frippy-blue p-4">Tailwind 작동 확인2</div>
 }
