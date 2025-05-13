f60e2fc3b6f4b7d9135c67e6284ff7891db4afaa
Jugwang <hjjk0721@gmail.com>
2025-05-13 13:27:15 +0900

    fix(tailwind): update for v4 and turborepo structure recommended

--- apps/client/src/app/page.tsx
+++ apps/client/src/app/page.tsx
@@ -1,3 +1,10 @@
 export default function Page() {
-  return <div className="text-3xl text-red-500">Tailwind 작동 확인</div>
+  return (
+    <>
+      <div className="text-3xl text-red-500">Tailwind 작동 확인</div>
+      <div className="text-2xl" style={{ color: 'var(--frippy-blue)' }}>
+        Tailwind 커스텀작동 확인
+      </div>
+    </>
+  )
 }
