4c27e038f65a1b1b16d02e5439cf978c90ef5ca2
Jugwang <hjjk0721@gmail.com>
2025-05-13 19:50:37 +0900

    feat(tailwind): set basic safelist for develop and extend features for service

--- apps/client/src/app/page.tsx
+++ apps/client/src/app/page.tsx
@@ -1,11 +1,11 @@
 export default function Page() {
   return (
     <>
-      <div className="text-3xl text-red-500">Tailwind 작동 확인</div>
+      <div className="text-xl text-red-500">Tailwind 작동 확인</div>
       <div className="text-2xl" style={{ color: 'var(--frippy-blue)' }}>
         Tailwind 커스텀작동 확인
       </div>
-      <div className="text-xl" style={{ color: 'var(--frippy-blue)' }}>
+      <div className="text-3xl" style={{ color: 'var(--frippy-blue)' }}>
         Tailwind 커스텀작동 확인
       </div>
     </>
