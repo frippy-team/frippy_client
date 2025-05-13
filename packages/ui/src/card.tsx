3438d53befd52fe96d0c0a1f316be1244c52a7c4
Jugwang <hjjk0721@gmail.com>
2025-05-13 19:25:34 +0900

    fix(tailwind): update packages/ui structure and useage for common components

--- packages/ui/src/card.tsx
+++ packages/ui/src/card.tsx
@@ -18,10 +18,11 @@ export function Card({
     >
       <h2 className="mb-3 text-2xl font-semibold">
         {title}{' '}
-        <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+        <span className="text-xl inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
           -&gt;
         </span>
       </h2>
+      <div className="text-3xl text-red-500 font-bold">Card 컴포넌트</div>
       <p className="m-0 max-w-[30ch] text-sm opacity-50">{children}</p>
     </a>
   )
